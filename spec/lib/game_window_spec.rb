ca69bced9d30208080fe2609151f630d7ac87c05
LJ Nissen <lj.nissen@gmail.com>
2017-12-06 17:52:40 +0100

    Make game_window spec work

--- spec/lib/game_window_spec.rb
+++ spec/lib/game_window_spec.rb
@@ -1,2 +1,13 @@
 require_relative "../spec_helper"
 
+
+
+describe GameWindow do
+	let(:game_window) 		{ GameWindow.new }
+
+	describe ".new" do
+		it "returns a GameWindow object" do
+			expect(game_window).to be_an_instance_of GameWindow
+		end
+	end
+end
